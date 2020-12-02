6086bf6de24320d5066924a3f1dc9cf6938813cd
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-12-02 17:05:59 -0300

    updates

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import logo from '../../assets/github-logo.svg';
+
 import { Title } from './style';
 
 const Dashborad: React.FC = () => {
-  return <Title>Explore Repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore Repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashborad;
