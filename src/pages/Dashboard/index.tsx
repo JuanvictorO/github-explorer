8cc200a5eb8e3add2ab19e7c7a1919aef2acd6eb
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-12-07 16:57:47 -0300

    first react project

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/github-logo.svg';
 
-import { Title } from './style';
+import { Title, Form, Repositories } from './style';
 
 const Dashborad: React.FC = () => {
   return (
@@ -11,9 +12,79 @@ const Dashborad: React.FC = () => {
       <Title>Explore Repositórios no Github</Title>
 
       <Form>
-        <input placeholder="Digite o nome do repositório" />
+        <input type="text" placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/28358198?s=460&u=73f41cf3b51573580de26daae5dcda82d17b25e2&v=4"
+            alt="Juan Victor Oliveira"
+          />
+          <div>
+            <strong>JuanvictorO/summernote</strong>
+            <p>
+              About Um repositório que implementa o summernote com o banco de
+              dados (incluindo adição de imagens e vídeos) com o Codeigniter 3
+              e, futuramente, 4
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/28358198?s=460&u=73f41cf3b51573580de26daae5dcda82d17b25e2&v=4"
+            alt="Juan Victor Oliveira"
+          />
+          <div>
+            <strong>JuanvictorO/summernote</strong>
+            <p>
+              About Um repositório que implementa o summernote com o banco de
+              dados (incluindo adição de imagens e vídeos) com o Codeigniter 3
+              e, futuramente, 4
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/28358198?s=460&u=73f41cf3b51573580de26daae5dcda82d17b25e2&v=4"
+            alt="Juan Victor Oliveira"
+          />
+          <div>
+            <strong>JuanvictorO/summernote</strong>
+            <p>
+              About Um repositório que implementa o summernote com o banco de
+              dados (incluindo adição de imagens e vídeos) com o Codeigniter 3
+              e, futuramente, 4
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars0.githubusercontent.com/u/28358198?s=460&u=73f41cf3b51573580de26daae5dcda82d17b25e2&v=4"
+            alt="Juan Victor Oliveira"
+          />
+          <div>
+            <strong>JuanvictorO/summernote</strong>
+            <p>
+              About Um repositório que implementa o summernote com o banco de
+              dados (incluindo adição de imagens e vídeos) com o Codeigniter 3
+              e, futuramente, 4
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
